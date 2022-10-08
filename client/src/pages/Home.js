15192b4c04d3c295cd851946c0340674fe5a518f
roohan25 <rohansasne30@gmail.com>
2022-10-08 22:33:58 +0530

    New Features added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React, {useEffect } from 'react' 
+import jwt_decode from "jwt-decode";
+
+const Home = () => {
+    
+
+        
+        
+
+    
+        
+            
+        return (
+            <div>
+                <h1>Home</h1>
+            </div>
+        )
+    }
+
+export default Home
